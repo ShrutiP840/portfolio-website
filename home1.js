7fc61a5338cd0b4dd3b5f740a39cc417ec9d8110
Shruti Prajapati <211240116054.it@gmailcom>
2025-11-19 16:19:07 +0530

    update in js file

--- home1.js
+++ home1.js
@@ -1,22 +1,10 @@
 // MOBILE MENU TOGGLE
 let menuBtn = document.getElementById("menu-btn");
-let navLinks = document.querySelector(".nav-links");
+let navLinks = document.querySelector(".navlinks");
 
 menuBtn.addEventListener("click", () => {
     navLinks.style.display =
         navLinks.style.display === "flex" ? "none" : "flex";
 });
 
-// SCROLL ANIMATION
-const animateElements = document.querySelectorAll(".animate");
-
-window.addEventListener("scroll", () => {
-    animateElements.forEach(el => {
-        let position = el.getBoundingClientRect().top;
-        let screenHeight = window.innerHeight;
-
-        if (position < screenHeight - 100) {
-            el.classList.add("show");
-        }
-    });
-});
+// git
