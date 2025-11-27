9c9440768e8cde9742c0d593b6170896eafe59bf
Shruti Prajapati <211240116054.it@gmailcom>
2025-11-27 14:26:59 +0530

    add footer in html

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ menuBtn.addEventListener("click", () => {
         navLinks.style.display === "flex" ? "none" : "flex";
 });
 
+/*document.querySelectorAll(".navlinks a").forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    const headerHeight = document.querySelector("header").offsetHeight;
+
+    const position = target.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+
+    window.scrollTo({
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
+*/
+
+
+
 
